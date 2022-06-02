d686792908500e07915bd6adf4eab5e49ed7f575
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-06-02 22:41:33 +0200

    clean code + j'aime

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -15,7 +15,7 @@ import 'moment/locale/fr';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
@@ -52,14 +52,18 @@ const Post = ({ post, setCurrentId }) => {
         {post.title}
       </Typography>
       <CardContent>
-        <Typography variant="h5" gutterBottom>
+        <Typography variant="body2" color="textSecondary" component="p">
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
           <ThumbUpAltIcon fontSize="small" />
-          J'aime
+          &nbsp; J'aime &nbsp;
           {post.likeCount}
         </Button>
         <Button
--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -13,12 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNEXION_URL =
-  'mongodb+srv://KevinO:ShenZhen%40518@cluster0.8litn.mongodb.net/?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNEXION_URL)
+  .connect(process.env.CONNEXION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Serveur connecté sur le port: ${PORT}`))
   )
